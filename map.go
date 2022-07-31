3bb822a178479ff7c08ec2618fcdcba3bc5abc60
Scott <ynot2718@gmail.com.au>
2022-08-01 00:06:51 +1000

    Added comma ok syntax

--- map.go
+++ map.go
@@ -14,4 +14,25 @@ func main() {
 	temperature["Earth"] = 16
 	temperature["Venus"] = 464
 	fmt.Println(temperature)
+	// Now try and read a key that doesnt exist should give 0
+	moon := temperature["Moon"]
+	fmt.Println(moon)
+	// Comma ok syntax to test a key
+	if moon, ok := temperature["Moon"]; ok {
+		fmt.Printf("On average the moon is %vo C.\n", moon)
+	} else {
+		fmt.Println("Where is the moon?")
+	}
+	// we can delete a key
+	planets := map[string]string{
+		"Earth": "Sector ZZ9",
+		"Mars":  "Sector ZZ9",
+	}
+	planetsMarkII := planets
+
+	planets["Earth"] = "whoops"
+	fmt.Println(planets)
+	fmt.Println(planetsMarkII)
+	delete(planets, "Earth")
+	fmt.Println(planetsMarkII)
 }
