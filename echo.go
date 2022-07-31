5d605e430c4ee3b1f8267fe2e9445e5ce9e6f641
Scott <ynot2718@gmail.com.au>
2022-07-31 17:57:40 +1000

    moved some code

---
+++ echo.go
@@ -0,0 +1,16 @@
+// From The Go Programming Language
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	s, sep := "", ""
+	for _, arg := range os.Args[0:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+}
